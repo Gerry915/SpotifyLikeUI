8259bddd159df951ff12cad4b18078a74021ae8b
Gerry915 <gao88915@gmail.com>
2022-05-09 12:10:43 +1000

    Initial Commit

---
+++ SpotifyUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyUI
+//
+//  Created by Gerry Gao on 9/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
